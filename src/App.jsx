a81ad372e7e78848c8a3a60ab1f5539e2974de3c
Sihab Hossain <sihabhossain380@gmail.com>
2023-07-11 00:48:18 +0600

    top bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButton from "./Components/TopButton";
+import Inputs from "./Components/Inputs";
+import TimeAndLocation from "./Components/TimeAndLocation";
 
 const App = () => {
   return (
-    <div className="text-5xl text-red-600">
-      <UilReact size="140" color="#61DAFB" />
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButton></TopButton>
+      <Inputs></Inputs>
+
+      <TimeAndLocation></TimeAndLocation>
     </div>
   );
 };
