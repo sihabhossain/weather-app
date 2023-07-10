4a3f1bab2980e13c5e59f428fdff40ffe7429fbb
Sihab Hossain <sihabhossain380@gmail.com>
2023-07-10 15:15:30 +0600

    icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 const App = () => {
-  return <div className="text-5xl text-red-600">Hello world</div>;
+  return (
+    <div className="text-5xl text-red-600">
+      <UilReact size="140" color="#61DAFB" />
+    </div>
+  );
 };
 
 export default App;
